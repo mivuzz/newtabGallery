6cad05269497c297fb0a94ccd2e55e5e8100ebdd
kytev <iaintgivingmyemail@cock.li>
2022-05-29 16:08:42 -0500

    v1

---
+++ renameImgs.py
@@ -0,0 +1,11 @@
+import os
+os.chdir('/home/imu/Documents/code/firefox_newtab/media/')
+it = 0
+for f in os.listdir():
+    filename,filext = os.path.splitext(f)
+    if filext == ".png" or filext == ".jpg" or filext == "jpeg":
+        print(filename, filext, "~image")
+        newName = "img_" + str(it) + filext
+        print(newName)
+        os.rename(f, newName)
+        it += 1

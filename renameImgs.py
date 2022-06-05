f0df3a9ffc875ebf495b8d44265a9d9677cc63da
JosephDFG <josephdfgx@gmail.com>
2022-06-05 11:48:09 -0500

    v2

--- renameImgs.py
+++ renameImgs.py
@@ -5,7 +5,7 @@ for f in os.listdir():
     filename,filext = os.path.splitext(f)
     if filext == ".png" or filext == ".jpg" or filext == "jpeg":
         print(filename, filext, "~image")
-        newName = "img_" + str(it) + filext
+        newName = "img__" + str(it) + filext
         print(newName)
         os.rename(f, newName)
         it += 1

a23ccc57762e86d74b67105572ccc1a06880cee1
zu <castleup@pm.me>
2022-10-27 13:57:00 -0500

    v?

--- renameImgs.py
+++ renameImgs.py
@@ -4,8 +4,8 @@ it = 0
 for f in os.listdir():
     filename,filext = os.path.splitext(f)
     if filext == ".png" or filext == ".jpg" or filext == "jpeg":
-        print(filename, filext, "~image")
-        newName = "img_" + str(it) + filext
+        print(filename + filext, "~image")
+        newName = "img" + str(it) + filext
         print(newName)
         os.rename(f, newName)
         it += 1
